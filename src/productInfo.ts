8acbb93e2674142c9676ff7f2710677a033fa435
Il Harper <afanyiyu@hotmail.com>
2021-06-28 20:29:31 +0800

    refactor: Product Info Update

--- src/productInfo.ts
+++ src/productInfo.ts
@@ -1,70 +1,76 @@
-interface AdobeProduct {
+export interface AdobeProductVersion {
+  id: string
   name: string
-  versions: { [key: string]: string }
 }
 
-export const products: { [key: string]: AdobeProduct } = {
-  AfterEffects: {
-    name: 'After Effects',
-    versions: {
-      '8.0': 'CS3 (8.0)',
-      '9.0': 'CS4 (9.0)',
-      '10.0': 'CS5 (10.0)',
-      '10.5': 'CS5.5 (10.5)',
-      '11.0': 'CS6 (11.0)',
-      '12.0': 'CC (12.0)',
-      '12.1': 'CC (12.1)',
-      '12.2': 'CC (12.2)',
-      '13.0': 'CC 2014 (13.0)',
-      '13.1': 'CC 2014.1 (13.1)',
-      '13.5': 'CC 2015 (13.5)',
-      '13.6': 'CC 2015.1 (13.6)',
-      '13.7': 'CC 2015.2 (13.7)',
-      '13.8': 'CC 2015.3 (13.8)',
-      '14.1': 'CC 2017.1 (14.1)',
-      '14.2': 'CC 2017.2 (14.2)',
-      '15.0': 'CC (15.0)',
-      '15.1': 'CC (15.1)',
-      '16.0': 'CC (16.0)',
-    },
-  },
-  Animate: {
-    name: 'Animate',
-    versions: {
-      '2013': 'CC 2013',
-    },
-  },
-  Audition: {
-    name: 'Audition',
-    versions: {
-      '2015.2': 'CC 2015.2',
-      '2017': 'CC 2017',
-      '2018': 'CC 2018',
-    },
-  },
-  Illustrator: {
-    name: 'Illustrator',
-    versions: {
-      '2015.3': 'CC 2015.3',
-    },
-  },
-  InDesign: {
-    name: 'InDesign',
-    versions: {
-      '2015.3': 'CC 2015.3',
-      '2018': 'CC 2018',
-    },
-  },
-  Photoshop: {
-    name: 'Photoshop',
-    versions: {
-      '2015.5': 'CC 2015.5',
-    },
-  },
-  Premiere: {
-    name: 'Premiere',
-    versions: {
-      '2018': 'CC 2018',
-    },
-  },
+export interface AdobeProduct {
+  id: string
+  name: string
+  versions: AdobeProductVersion[]
 }
+
+export const products: AdobeProduct[] = [
+  {
+    id: 'AfterEffects',
+    name: 'After Effects',
+    versions: [
+      { id: '8.0', name: 'CS3 (8.0)' },
+      { id: '9.0', name: 'CS4 (9.0)' },
+      { id: '10.0', name: 'CS5 (10.0)' },
+      { id: '10.5', name: 'CS5.5 (10.5)' },
+      { id: '11.0', name: 'CS6 (11.0)' },
+      { id: '12.0', name: 'CC (12.0)' },
+      { id: '12.1', name: 'CC (12.1)' },
+      { id: '12.2', name: 'CC (12.2)' },
+      { id: '13.0', name: 'CC 2014 (13.0)' },
+      { id: '13.1', name: 'CC 2014.1 (13.1)' },
+      { id: '13.5', name: 'CC 2015 (13.5)' },
+      { id: '13.6', name: 'CC 2015.1 (13.6)' },
+      { id: '13.7', name: 'CC 2015.2 (13.7)' },
+      { id: '13.8', name: 'CC 2015.3 (13.8)' },
+      { id: '14.1', name: 'CC 2017.1 (14.1)' },
+      { id: '14.2', name: 'CC 2017.2 (14.2)' },
+      { id: '15.0', name: 'CC (15.0)' },
+      { id: '2018', name: 'CC 2018 (15.0)' },
+      { id: '15.1', name: 'CC (15.1)' },
+      { id: '16.0', name: 'CC (16.0)' },
+    ],
+  },
+  {
+    id: 'Animate',
+    name: 'Animate',
+    versions: [{ id: '2013', name: 'CC 2013' }],
+  },
+  {
+    id: 'Audition',
+    name: 'Audition',
+    versions: [
+      { id: '2017', name: 'CC 2017' },
+      { id: '2018', name: 'CC 2018' },
+      { id: '2015.2', name: 'CC 2015.2' },
+    ],
+  },
+  {
+    id: 'Illustrator',
+    name: 'Illustrator',
+    versions: [{ id: '2015.3', name: 'CC 2015.3' }],
+  },
+  {
+    id: 'InDesign',
+    name: 'InDesign',
+    versions: [
+      { id: '2018', name: 'CC 2018' },
+      { id: '2015.3', name: 'CC 2015.3' },
+    ],
+  },
+  {
+    id: 'Photoshop',
+    name: 'Photoshop',
+    versions: [{ id: '2015.5', name: 'CC 2015.5' }],
+  },
+  {
+    id: 'Premiere',
+    name: 'Premiere',
+    versions: [{ id: '2018', name: 'CC 2018' }],
+  },
+]
